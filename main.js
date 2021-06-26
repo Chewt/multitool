f3e0fd3a9edb4fa5c4ab0433d4f97e2cc4017410
Hayden Johnson <hajohn100@gmail.com>
2021-06-26 16:10:40 -0700

    changed from boomer var to zoomer let

--- main.js
+++ main.js
@@ -18,12 +18,13 @@ function has_im(string)
 client.on("ready", () =>
     {
         console.log(`Logged in as ${client.user.tag}!`)
-        client.user.setActivity('!helpme')
+        client.user.setActivity('your mom')
+        client.channels.cache.get('353708960777371648').send('Look pa I\'m alive!');
     })
 
 client.on("message", msg =>
     {
-        var words = msg.content.split(' ')
+        let words = msg.content.split(' ')
         if (!msg.author.bot && msg.content === "!compliment")
         {
             msg.channel.send(`Fuck you ${msg.author}`)
@@ -31,7 +32,7 @@ client.on("message", msg =>
         else if (!msg.author.bot && words[0] === "!vote")
         {
             votelist = msg.content.split(';')
-            var string = ""
+            let string = ""
             for (i = 1; i < votelist[0].split(' ').length; i++)
             {
                 string = string.concat(votelist[0].split(' ')[i], " ")
@@ -46,9 +47,9 @@ client.on("message", msg =>
         }
         else if (!msg.author.bot && (has_im(words) >= 0))
         {
-            var index = has_im(words)
+            let index = has_im(words)
             console.log("== I'm found at ", index)
-            var string = "Hi "
+            let string = "Hi "
             for (i = index + 1; i < words.length - 1; i++)
             {
                 string = string.concat(words[i], " ")
@@ -56,7 +57,7 @@ client.on("message", msg =>
             string = string.concat(words[words.length - 1])
             if (words[1] == "multitool" || words[1] == "Multitool")
             {
-                string = string.concat(", I'm... Wait a minute!")
+                string = string.concat(`, I'm ${msg.author}!`)
             }
             else
             {
@@ -64,7 +65,7 @@ client.on("message", msg =>
             }
             msg.channel.send(string)
         }
-        else if (msg.content === "!helpme")
+        else if (msg.content === "!help")
         {
             msg.channel.send("You can create a vote by using the command\n!vote"
                 + " item 1;item 2;item 3 ...\n")
